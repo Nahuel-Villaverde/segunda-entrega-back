c970e9effe9112d0c34e824e6760bf2f9c90acb7
Nahuel Villaverde <nahuelvillaverdeguzman@gmail.com>
2024-05-20 15:16:58 -0300

    endpoints-products-terminados

--- src/routes/api/products.router.js
+++ src/routes/api/products.router.js
@@ -8,14 +8,19 @@ router.get('/', async (req, res) => {
     let limit = parseInt(req.query.limit) || 10;
     let categoria = req.query.categoria;
     let sort = req.query.sort;
+    let disponible = req.query.disponible;
 
     console.log(`Limite establecido: ${limit}`);
     console.log(`Parámetro de ordenamiento recibido: ${sort}`);
-    
+    console.log(`Parámetro de disponibilidad recibido: ${disponible}`);
+
     try {
         let filtro = {};
         if (categoria) {
             filtro.categoria = categoria;
+        }
+        if (disponible !== undefined) {
+            filtro.disponible = disponible === 'true'; // Convertir a booleano
         }
 
         let ordenamiento = {};
@@ -26,14 +31,27 @@ router.get('/', async (req, res) => {
         }
 
         console.log('Objeto de ordenamiento:', ordenamiento);
+        console.log('Filtro:', filtro);
 
         const result = await productModel.paginate(filtro, { page, limit, lean: true, sort: ordenamiento });
 
-        console.log(result);
-        const prevLink = result.hasPrevPage ? `/api/products?page=${result.prevPage}&limit=${limit}&categoria=${categoria || ''}&sort=${sort || ''}` : null;
-        const nextLink = result.hasNextPage ? `/api/products?page=${result.nextPage}&limit=${limit}&categoria=${categoria || ''}&sort=${sort || ''}` : null;
+        const prevLink = result.hasPrevPage ? `/api/products?page=${result.prevPage}&limit=${limit}&categoria=${categoria || ''}&sort=${sort || ''}${disponible !== undefined ? `&disponible=${disponible}` : ''}` : null;
+        const nextLink = result.hasNextPage ? `/api/products?page=${result.nextPage}&limit=${limit}&categoria=${categoria || ''}&sort=${sort || ''}${disponible !== undefined ? `&disponible=${disponible}` : ''}` : null;
 
-        res.render('products', { products: result.docs, page, limit, prevLink, nextLink, categoria, sort });
+        console.log('Resultados:', {
+            status: 'success',
+            payload: result.docs,
+            totalPages: result.totalPages,
+            prevPage: result.prevPage,
+            nextPage: result.nextPage,
+            page: result.page,
+            hasPrevPage: result.hasPrevPage,
+            hasNextPage: result.hasNextPage,
+            prevLink: prevLink,
+            nextLink: nextLink
+        });
+
+        res.render('products', { products: result.docs, page, limit, prevLink, nextLink, categoria, sort, disponible });
     } catch (error) {
         console.error('Error al obtener los productos:', error);
         res.status(500).render('error', { error: 'Error al obtener los productos' });
@@ -59,7 +77,7 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
     let { titulo, descripcion, precio, thumbnail, categoria, code, stock, disponible } = req.body
 
-    if ( !titulo || !descripcion || !precio || !thumbnail || !categoria || !code || !stock || !disponible) {
+    if (!titulo || !descripcion || !precio || !thumbnail || !categoria || !code || stock === undefined || disponible === undefined) {
         return res.send({ status: "error", error: "Faltan parametros" })
     }
 
